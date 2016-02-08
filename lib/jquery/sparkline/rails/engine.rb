e06246c91ff49b93492ecc3d5dca4f07cf873899
gaurav-netspi <gaurav.gaur@netspi.com>
2016-02-08 16:54:11 -0600

    Update engine.rb

--- lib/jquery/sparkline/rails/engine.rb
+++ lib/jquery/sparkline/rails/engine.rb
@@ -2,11 +2,11 @@ require 'rails'
 
 module Jquery
   module Sparkline
-    #if defined?(::Rails) and Gem::Version.new(::Rails.version.to_s) >= Gem::Version.new("4.0")
+    if defined?(::Rails) and Gem::Version.new(::Rails.version.to_s) >= Gem::Version.new("4.0")
       module Rails
         class Engine < ::Rails::Engine
         end
       end
-    #end
+    end
   end
 end
